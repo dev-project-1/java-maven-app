208b542dd20d7605ea9bd8ec8038cef6fd2743f5
zenintoji07 <151115994+zenintoji07@users.noreply.github.com>
2023-11-21 10:41:42 +0500

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,14 +1,19 @@
-def buildApp() {
-    echo 'building the application'
+def buildJar() {
+    echo "building the application"
+    sh 'mvn package'
+}
+def buildImage() {
+    echo "building the docker image"
+                    withCredentials([usernamePassword(credentialsID: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                        sh 'docker build -t zenintoji/my-repo:jma-2.0 .'
+                        sh "echo $PASS | docker login -u $USER --password-stdin"
+                        sh 'docker push zenintoji/my-repo:jma-2.0'
+                    }
 }
 
-def testApp() {
-    echo 'building the application'
-}
 
 def deployApp() {
     echo 'deploying the application'
-    echo "deploying version ${params.VERSION}"
 }
 
 return this 
